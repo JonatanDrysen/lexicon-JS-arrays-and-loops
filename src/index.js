75df1f40dbda73780e206c960d9b36fc996f79b2
JonatanDrysen <jojodrysen@gmail.com>
2024-01-16 15:28:55 +0100

    add task 3

--- src/index.js
+++ src/index.js
@@ -5,4 +5,14 @@ console.log("task 1: ", arr)
 
 // task 2
 const numbers = [123, 6, 1, 0, 12, 53]
-console.log(numbers.map((n) => n * 3))
+console.log("task 2: ", numbers.map((n) => n * 3))
+
+// task 3
+const numbers2 = [1, 2, 56, 3, 0, 81, 5, 7, 9]
+const newArr= []
+numbers2.forEach((item) => {
+    if(item >= 5) {
+        newArr.push(item)
+    }
+})
+console.log("task 3: ", newArr)
