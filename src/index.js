d55ebb8654cdd5731711937fe39dd24bbd83e9f3
JonatanDrysen <jojodrysen@gmail.com>
2024-01-16 15:01:14 +0100

    add task 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // task 1
 const arr = []
 arr.push("hi", "my", "name", "is", "jonis")
-console.log(arr)
+console.log("task 1: ", arr)
+
+// task 2
+const numbers = [123, 6, 1, 0, 12, 53]
+console.log(numbers.map((n) => n * 3))
